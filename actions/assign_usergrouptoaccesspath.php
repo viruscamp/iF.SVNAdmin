1aded8151ce5294769d2eb287efe5ccf0a0a8f3a
viruscamp <viruscamp@gmail.com>
2022-05-07 22:38:21 +0800

    fix #74 #144, filter out null and '' selected_users selected_groups and selected_accesspaths

--- actions/assign_usergrouptoaccesspath.php
+++ actions/assign_usergrouptoaccesspath.php
@@ -7,12 +7,14 @@ $selgroups = get_request_var('selected_groups');
 $selpaths  = get_request_var('selected_accesspaths');
 $selperm   = get_request_var('permission'); // TODO: There is no check, whether this var is given!
 
-if (count($selusers) == 1 && empty($selusers[0]))
-  $selusers = NULL;
-if (count($selgroups) == 1 && empty($selgroups[0]))
-  $selgroups = NULL;
-if (count($selpaths) == 1 && empty($selpaths[0]))
-  $selpaths = NULL;
+// filter out all null, '', and false but leave 0, '0'
+if ($selusers != NULL) $selusers = array_filter($selusers, 'strlen');
+if ($selgroups != NULL) $selgroups = array_filter($selgroups, 'strlen');
+if ($selpaths != NULL) $selpaths = array_filter($selpaths, 'strlen');
+
+if (count($selusers) == 0) $selusers = NULL;
+if (count($selgroups) == 0) $selgroups = NULL;
+if (count($selpaths) == 0) $selpaths = NULL;
 
 if( $selpaths == NULL || ( $selusers == NULL && $selgroups == NULL ) )
 {
@@ -21,82 +23,82 @@ if( $selpaths == NULL || ( $selusers == NULL && $selgroups == NULL ) )
 else
 {
 	try {
-	  // The number of selected elements.
-	  $selpathsLen = count($selpaths);
-	  $selgroupsLen = ($selgroups != NULL) ? count($selgroups) : 0;
-	  $selusersLen =  ($selusers  != NULL) ? count($selusers)  : 0;
+		// The number of selected elements.
+		$selpathsLen = count($selpaths);
+		$selgroupsLen = ($selgroups != NULL) ? count($selgroups) : 0;
+		$selusersLen =  ($selusers  != NULL) ? count($selusers)  : 0;
 
-	  // Create permission object.
-	  $oP = new \svnadmin\core\entities\Permission;
-	  $oP->perm = $selperm;
+		// Create permission object.
+		$oP = new \svnadmin\core\entities\Permission;
+		$oP->perm = $selperm;
 
-	  // Iterate all selected_accesspaths.
-	  for( $i=0; $i<$selpathsLen; $i++ )
-	  {
-	    $oAP = new \svnadmin\core\entities\AccessPath;
-	    $oAP->id = $selpaths[$i];
-	    $oAP->path = $selpaths[$i];
+		// Iterate all selected_accesspaths.
+		foreach($selpaths as $selpath)
+		{
+			$oAP = new \svnadmin\core\entities\AccessPath;
+			$oAP->id = $selpath;
+			$oAP->path = $selpath;
 
-	    // Is the user restricted to some paths? (project-manager)
-	    if ($appEngine->isAuthenticationActive())
-	    {
-	      $currentUsername = $appEngine->getSessionUsername();
-	      if ($appEngine->getAclManager()->isUserAccessPathManager($currentUsername))
-	      {
-	        if (!$appEngine->getAclManager()->isUserAdminOfPath($currentUsername, $oAP->path))
-	        {
-	          // Skip assignment.
-	          $appEngine->addException(new Exception(tr("No administration permission for %0", array($oAP->path))));
-	          continue;
-	        }
-	      }
-	    }
+			// Is the user restricted to some paths? (project-manager)
+			if ($appEngine->isAuthenticationActive())
+			{
+				$currentUsername = $appEngine->getSessionUsername();
+				if ($appEngine->getAclManager()->isUserAccessPathManager($currentUsername))
+				{
+					if (!$appEngine->getAclManager()->isUserAdminOfPath($currentUsername, $oAP->path))
+					{
+						// Skip assignment.
+						$appEngine->addException(new Exception(tr("No administration permission for %0", array($oAP->path))));
+						continue;
+					}
+				}
+			}
 
-		if ( $selusersLen > 0 || $selgroupsLen > 0 ) {
-			$b = $appEngine->getAccessPathEditProvider()->createAccessPath($oAP);
+			if ( $selusersLen > 0 || $selgroupsLen > 0 ) {
+				$b = $appEngine->getAccessPathEditProvider()->createAccessPath($oAP);
+			}
+
+			// Iterate selected_users.
+			foreach($selusers as $seluser)
+			{
+				$oU = new \svnadmin\core\entities\User;
+				$oU->id = $seluser;
+				$oU->name = $seluser;
+
+				try {
+					$b = $appEngine->getAccessPathEditProvider()->assignUserToAccessPath($oU, $oAP, $oP);
+					if (!$b) {
+						throw new Exception("ERROR");
+					}
+					$appEngine->addMessage(tr("Grant %0 permission to %1 on %2", array($oP->perm, $oU->name, $oAP->path)));
+				}
+				catch (Exception $e) {
+					$appEngine->addException($e);
+				}
+			}
+
+			// Iterate selected_groups.
+			foreach($selgroups as $selgroup)
+			{
+				$oG = new \svnadmin\core\entities\Group;
+				$oG->id = $selgroup;
+				$oG->name = $selgroup;
+
+				try {
+					$b = $appEngine->getAccessPathEditProvider()->assignGroupToAccessPath( $oG, $oAP, $oP );
+					if (!$b) {
+						throw new Exception("ERROR");
+					}
+					$appEngine->addMessage(tr("Grant %0 permission to %1 on %2", array($oP->perm, $oG->name, $oAP->path)));
+				}
+				catch (Exception $e) {
+					$appEngine->addException($e);
+				}
+			}
 		}
 
-		// Iterate selected_users.
-		for( $iu=0; $iu<$selusersLen; $iu++ )
-    	{
-			$oU = new \svnadmin\core\entities\User;
-			$oU->id = $selusers[$iu];
-			$oU->name = $selusers[$iu];
-
-			try {
-				$b = $appEngine->getAccessPathEditProvider()->assignUserToAccessPath($oU, $oAP, $oP);
-				if (!$b) {
-					throw new Exception("ERROR");
-				}
-				$appEngine->addMessage(tr("Grant %0 permission to %1 on %2", array($oP->perm, $oU->name, $oAP->path)));
-			}
-			catch (Exception $e) {
-				$appEngine->addException($e);
-			}
-    	}
-
-	    // Iterate selected_groups.
-	    for( $ig=0; $ig<$selgroupsLen; $ig++ )
-	    {
-	      $oG = new \svnadmin\core\entities\Group;
-	      $oG->id = $selgroups[$ig];
-	      $oG->name = $selgroups[$ig];
-
-	      try {
-	      	$b = $appEngine->getAccessPathEditProvider()->assignGroupToAccessPath( $oG, $oAP, $oP );
-	      	if (!$b) {
-	      		throw new Exception("ERROR");
-	      	}
-	      	$appEngine->addMessage(tr("Grant %0 permission to %1 on %2", array($oP->perm, $oG->name, $oAP->path)));
-	      }
-	      catch (Exception $e) {
-	      	$appEngine->addException($e);
-	      }
-	    }
-	  }
-
-	  // Save changes!
-	  $appEngine->getAccessPathEditProvider()->save();
+		// Save changes!
+		$appEngine->getAccessPathEditProvider()->save();
 	}
 	catch (Exception $ex) {
 		$appEngine->addException($ex);
